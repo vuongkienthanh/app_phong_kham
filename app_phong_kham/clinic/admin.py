f43f46c502eb954cf483dfb8c58706ca8e0eff89
vuongkienthanh <thanhstardust@outlook.com>
2020-04-27 07:21:38 +0700

    change to core

--- app_phong_kham/clinic/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Patient, Visit, DrugWarehouse, LineDrug, VisitQueue
-# Register your models here.
-
-admin.site.register([Patient, Visit, DrugWarehouse, LineDrug, VisitQueue])
